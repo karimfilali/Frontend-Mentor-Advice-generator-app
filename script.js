0a2245d51c7b256718b05541a89fd8e65348427b
Karim F <ah.filali93@gmail.com>
2022-02-20 23:41:52 +0100

    Deleted cache in fetch method and currentAdvice variable

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 const diceBtn = document.querySelector(".dice")
 const adviceNumber = document.querySelector(".advice_number")
 const adviceDiv = document.querySelector(".advice")
-let currentAdvice = ""
 diceBtn.addEventListener("click", () => getAdvice())
 
 getAdvice()
 
 function getAdvice(){
-    fetch("https://api.adviceslip.com/advice").then(response => {
+    fetch("https://api.adviceslip.com/advice", { cache: "no-cache", }).then(response => {
         return response.json()
     }).then(adviceData => {
         const AdviceObject = adviceData.slip
         adviceDiv.innerText = AdviceObject.advice
-        if(AdviceObject.advice != currentAdvice)
-            adviceNumber.innerText = `Advice # ${Math.floor(Math.random() * 200)}`
-        currentAdvice = AdviceObject.advice
+        adviceNumber.innerText = `Advice # ${Math.floor(Math.random() * 200)}`
     }).catch(error => {
         console.log(error)
     })
